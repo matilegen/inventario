d22b872202a97ed0e82fc5e880a6605527ecda69
Matias Alejandro Serantes <48970043+matilegen@users.noreply.github.com>
2023-12-19 15:36:17 -0300

    sss

---
+++ kotlin/Usuario.kt
@@ -0,0 +1,7 @@
+class Usuario{
+    val nombre: String= ""
+    val apellido String= ""
+    var fechaIngreso : LocalDate
+    val inicioTurno: Int=08
+    val finTurno:Int=17
+}
---
+++ kotlin/elementos.kt
@@ -0,0 +1,6 @@
+class Elementos() {
+    var serial:Int=0
+    var nombreProd:String
+    var fechaIngreso: LocalDate
+    var usuario: Usuario
+}
